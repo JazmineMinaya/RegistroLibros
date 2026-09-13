b370b7dd342823b427734ce897466f2ad1c62298
Jazmine Arleny Minaya Peralta <peraltajasmine2121@gmail.com>
2026-09-13 01:51:09 -0400

    Proyecto RegistroLibros: Guardando la creación de la clase Libro, el Contexto y el ConnectionString

---
+++ RegistroLibros/Context/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using RegistroLibros.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace RegistroLibros.Context
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Libro> Libros { get; set; }
+    }
+}
---
+++ RegistroLibros/Models/Libro.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroLibros.Models
+{
+    public class Libro
+    {
+        [Key]
+        public int LibroId { get; set; }
+
+        [Required(ErrorMessage = "El título es obligatorio")]
+        public string Titulo { get; set; } = "";
+
+        [Required(ErrorMessage = "El autor es obligatorio")]
+        public string Autor { get; set; } = "";
+
+        [Required(ErrorMessage = "El año de publicación es obligatorio")]
+        public int AnoPublicacion { get; set; }
+    }
+}
