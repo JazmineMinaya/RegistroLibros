fcb06834805259dac88e93969cfd0b65096e1dbf
Jazmine Arleny Minaya Peralta <peraltajasmine2121@gmail.com>
2026-09-14 22:58:05 -0400

    Agregando entidades Jugador y Partida para el Branch

---
+++ RegistroLibros/Models/Jugador.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroLibros.Models;
+
+public class Jugador
+{
+    [Key]
+    public int JugadorId { get; set; }
+
+    [Required]
+    public string Nombre { get; set; } = "";
+
+    public ICollection<Partida> Partidas { get; set; } = new List<Partida>();
+}
---
+++ RegistroLibros/script.sql
@@ -0,0 +1,25 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+CREATE TABLE [Libros] (
+    [LibroId] int NOT NULL IDENTITY,
+    [Titulo] nvarchar(max) NOT NULL,
+    [Autor] nvarchar(max) NOT NULL,
+    [AnoPublicacion] int NOT NULL,
+    CONSTRAINT [PK_Libros] PRIMARY KEY ([LibroId])
+);
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260913083210_Inicial', N'10.0.12');
+
+COMMIT;
+GO
+
